9843bf56ca8e070d9c1ad812bf06b2b7ee42efd8
Shivaniguptha <96287240+Shivaniguptha@users.noreply.github.com>
2022-10-27 18:46:56 +0530

    Update Delete_element_in_an_unsorted_array.py

--- Searching/Delete_element_in_an_unsorted_array.py
+++ Searching/Delete_element_in_an_unsorted_array.py
@@ -3,8 +3,6 @@
 arr=[1,5,8,3]
 num=int(input('Enter a number that has to be deleted from the array: '))
 
-for i in range(0,len(arr)-1):
-    if arr[i]==num:
-        arr.remove(arr[i])
+arr.remove(arr[i])
 
 print('{} is deleted from array and the final array is {}'.format(num,arr))
